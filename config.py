885df9f87371d975b8ce983fcc97ab4a45ae9aac
Carlos_Sagastume <care_sagastumea@unicah.edu>
2026-03-05 22:00:00 -0600

    Pequeño Cambio En Config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-# Configuración de la base de datos MySQL
+# Configuracion De DB En Mysql
 DB_CONFIG = {
     'host': 'localhost',
     'port': 3306,
     'user': 'root',
-    'password': '',  # Cambiar con la contraseña de MySQL
-    'database': 'inventory_management',
-    'raise_on_warnings': True
+    'password': '',  # Cambiar Con La Contraseña De Mysql De Su Computadora
+    'database': 'inventory_management', #Crear Database En Mysql, las tablas 
+                                        #se crearan automaticamente al ejecutar el programa
+    'raise_on_warnings': False   
 }
 
-# Ruta para guardar reportes
+# Ruta Por Defecto Para Guardar Los Reportes
 REPORTS_PATH = './reportes/'
